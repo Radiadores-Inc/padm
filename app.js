46070c80131c126000d8a5b5776aa65972ad20a8
Juan Pablo Gaviria <jgavirias13@gmail.com>
2020-08-19 20:54:10 -0500

    [InitialConfig] Configuracion inicial del servidor
    Se crea configuracion inicial del servidor con conexion a base de datos
    mongo.
    Se crea logger para unificar forma de debug
    Se crea middleware de error para capturar excepciones
    Se crea archivo de configuracion para tomar variables del archivo .env
    Se agrega archivo .sample-env con el fin de mantener un ejemplo de las
    variables que se deben inicializar
    Se eliminan archivos dummy de la carpeta server
    Se modifica archivo index routes con el fin de mostrar la configuracion
    que se debe realizar
    Se crea rutas de comunicados con el fin de ejemplificar forma de montar
    una ruta

---
+++ app.js
@@ -0,0 +1,30 @@
+/*
+* Main de la aplicacion
+* Este archivo es el que se encarga de ejecutar el servidor de express
+*/
+
+// Librerias
+const express = require('express');
+const logger = require('./src/util/logger');
+const config = require('./src/config/config');
+const db = require('./src/server/db');
+const routing = require('./src/routes');
+const errorMiddleware = require('./src/middlewares/errorMiddleware');
+require('express-async-errors');
+
+const app = express();
+const router = express.Router();
+
+routing(router);
+app.use('/api/v1', router);
+app.use(errorMiddleware)
+
+db.connect().then(() => {
+    logger.writeSuccess('Base de datos conectada exitosamente');
+}, err => {
+    logger.writeError(`Error al conectar con la base de datos: ${err.message}`);
+});
+
+app.listen(config.PORT, () => {
+    logger.writeSuccess(`${config.APPLICATION_NAME} ejecutandose en el puerto ${config.PORT}`);
+});
