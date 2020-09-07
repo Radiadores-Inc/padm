a3e782f8be4fa42e0a8277e7c00e9062252d330a
jjad85 <jjad85@gmail.com>
2020-09-06 20:41:50 -0500

    [property] Se adiciona el router para PROPERTY

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 /*
-* Archivo principal de rutas.
-* Sobre este archivo no se deben definir rutas, sino importarlas de su propio archivo de rutas
-*/
+ * Archivo principal de rutas.
+ * Sobre este archivo no se deben definir rutas, sino importarlas de su propio archivo de rutas
+ */
 const communicates = require('./communicatesRoutes');
+const property = require('./propertyRoutes');
 
 module.exports = (router) => {
     communicates(router);
-}
+    property(router);
+};
