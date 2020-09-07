f07de37e5db4639beff4a3d29465b3cbbdb5de4e
jjad85 <jjad85@gmail.com>
2020-09-06 20:40:52 -0500

    [property] Se adiciona el seteo del JSON

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
-* Main de la aplicacion
-* Este archivo es el que se encarga de ejecutar el servidor de express
-*/
+ * Main de la aplicacion
+ * Este archivo es el que se encarga de ejecutar el servidor de express
+ */
 
 // Librerias
 const express = require('express');
@@ -10,21 +10,30 @@ const config = require('./src/config/config');
 const db = require('./src/server/db');
 const routing = require('./src/routes');
 const errorMiddleware = require('./src/middlewares/errorMiddleware');
+const bodyParser = require('body-parser');
 require('express-async-errors');
 
 const app = express();
 const router = express.Router();
+app.use(bodyParser.json());
 
 routing(router);
 app.use('/api/v1', router);
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-db.connect().then(() => {
-    logger.writeSuccess('Base de datos conectada exitosamente');
-}, err => {
-    logger.writeError(`Error al conectar con la base de datos: ${err.message}`);
-});
+db.connect().then(
+    () => {
+        logger.writeSuccess('Base de datos conectada exitosamente');
+    },
+    (err) => {
+        logger.writeError(
+            `Error al conectar con la base de datos: ${err.message}`
+        );
+    }
+);
 
 app.listen(config.PORT, () => {
-    logger.writeSuccess(`${config.APPLICATION_NAME} ejecutandose en el puerto ${config.PORT}`);
+    logger.writeSuccess(
+        `${config.APPLICATION_NAME} ejecutandose en el puerto ${config.PORT}`
+    );
 });
